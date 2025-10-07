18871055a6cca287f577485edcf695cc807f207d
kd-kdev <dokmanac@live.com>
2025-10-07 19:18:30 +0200

    feat: add test route that extracts pydantic class from JSON response

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from crypt import methods
 from urllib import response
 import requests
+import json
 from flask import request, jsonify, render_template
 from config import app
+from pydantic import BaseModel
 
 
 @app.route("/")
@@ -22,9 +24,40 @@ def get_gameReviews():
 @app.route("/search_game/<int:game_id>", methods=["GET"])
 def search_game(game_id):
     game_id_string = str(game_id)
+    # potentially expand on this to add more options to the query ?
     link = "https://store.steampowered.com/appreviews/" + game_id_string + "?json=1"
     r = requests.get(link).content
     return r
+
+
+# Testing route for getting Pydantic from JSON
+# mapped to button for testing
+@app.route("/get_info/<int:game_id>", methods=["GET"])
+def get_info(game_id):
+    game_id_string = str(game_id)
+    # potentially expand on this to add more options to the query ?
+    link = "https://store.steampowered.com/appreviews/" + game_id_string + "?json=1"
+    response = requests.get(link)  # gets the JSON response from Steam's API
+
+    response_dict = (
+        response.json()
+    )  # parses the HTTP response body as JSON & returns a python dict
+
+    class QueryMeta(BaseModel):  # definition should be in it's own file
+        num_reviews: int
+        review_score: int
+        total_positive: int
+        total_negative: int
+        total_reviews: int
+
+    query_summary = response_dict.get(
+        "query_summary"
+    )  # extracts the 'query_summary' sub-dict from API response
+    parsed = QueryMeta(
+        **query_summary
+    )  # instanciates the Pydantic model using the 'query_summary' dict, pydantic validates field types & will coerce where appropriate
+
+    return parsed.model_dump_json(indent=2)
 
 
 if __name__ == "__main__":
