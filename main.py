94d427607730aaf88415bafeb68154e0f4b496b8
kd-kdev <dokmanac@live.com>
2025-10-07 14:45:15 +0200

    feat: add /search_game/<game_id> endpoint that works for searching games

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from urllib import response
 import requests
 from flask import request, jsonify, render_template
@@ -14,6 +15,15 @@ def get_gameReviews():
     r = requests.get(
         "https://store.steampowered.com/appreviews/292000?json=1&filter=recent"
     ).content
+    return r
+
+
+# Search functionality
+@app.route("/search_game/<int:game_id>", methods=["GET"])
+def search_game(game_id):
+    game_id_string = str(game_id)
+    link = "https://store.steampowered.com/appreviews/" + game_id_string + "?json=1"
+    r = requests.get(link).content
     return r
 
 
