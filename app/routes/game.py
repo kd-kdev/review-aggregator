32444d26f061e51b5b8f470b28a153524ce65886
kd-kdev <dokmanac@live.com>
2025-10-20 20:35:23 +0200

    feat(temp): made proof of concept game page fragment, moving away from HTMX & Jinja

--- app/routes/game.py
+++ app/routes/game.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from app.models import Game
+from flask import Blueprint, jsonify, render_template
+from app.models import Game, QuerySummary
 from app.schemas import GameSchema
 
 game_bp = Blueprint("game", __name__)
@@ -8,4 +8,15 @@ game_bp = Blueprint("game", __name__)
 @game_bp.route("/<int:game_id>", methods=["GET"])
 def get_game(game_id):
     game = Game.query.get_or_404(game_id)
-    return jsonify(GameSchema.model_validate(game).model_dump())
+    query = QuerySummary.query.get_or_404(game_id)
+
+    game_data = GameSchema.model_validate(game).model_dump()
+
+    filtered_data = {
+        "appid": game_data["appid"],
+        "name": game_data["name"],
+        "developers": game_data["developers"],
+        "publishers": game_data["publishers"],
+    }
+
+    return render_template("game.html", game=game, query=query)
